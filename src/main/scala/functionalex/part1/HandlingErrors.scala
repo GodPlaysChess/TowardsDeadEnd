fb9e58bef4892e48d5d64d560cda529ac5b85874
GodPlaysChess <pumabox@gmail.com>
2015-02-24 14:15:43 +0100

    Implemented sequence!!!

--- src/main/scala/functionalex/part1/HandlingErrors.scala
+++ src/main/scala/functionalex/part1/HandlingErrors.scala
@@ -1,8 +1,5 @@
 package functionalex.part1
 
-/**
- * Created by Gleb on 2/22/2015.
- */
 object HandlingErrors {
 
   def variance(xs: Seq[Double]): Option[Double] =
@@ -15,6 +12,22 @@ object HandlingErrors {
   def mean(xs: Seq[Double]): Option[Double] = {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.size)
+  }
+
+  def map2[A, B, C](oa: Option[A], ob: Option[B])(f: (A, B) => C): Option[C] =
+    for {
+      a <- oa
+      b <- ob
+    } yield f(a, b)
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    val acc = List[A]()
+    a.foldLeft(Some(acc): Option[List[A]])((acc, op) => map2(acc, op)((li, v) => li :+ v))
+  }
+
+  def main(args: Array[String]) {
+    println(sequence(List(Some(5), Some(6), Some(1))))
+    println(sequence(List(Some(5), None, Some(1))))
   }
 
 
