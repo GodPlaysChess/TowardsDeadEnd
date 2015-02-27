02644100b22feaeb9ca6dd9a6279c75712bb48d3
GodPlaysChess <pumabox@gmail.com>
2015-02-27 16:46:38 +0100

    +stream methods

--- src/main/scala/functionalex/part1/Stream.scala
+++ src/main/scala/functionalex/part1/Stream.scala
@@ -4,29 +4,50 @@ package functionalex.part1
  * Created by Gleb on 2/26/2015.
  */
 sealed trait Stream[+A] {
+
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
   }
 
-  // not sure about infering the A type in h.
   def toList: List[A] = this match {
     case Empty => Nil
-    case Cons(h: A, t: Stream[A]) => h :: t.toList
+    case Cons(h, t) => h() :: t().toList
   }
 
   def take(n: Int): List[A] = this match {
     case Empty => Nil
-    case Cons(h: A, t) => if (n == 0) Nil
-    else h :: take(n - 1)
+    case Cons(h, t) => if (n == 0) Nil
+    else h() :: t().take(n - 1)
   }
 
-
-  def drop(n: Int): Stream[A] = {
-    case Empty => Nil
-    case Cons(h: A, t) => if (n == 0) t
-    else t.apply().drop(n - 1)
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n != 1 => t().drop(n - 1)
+    case Cons(h, t) => t()
+    case Empty => Empty
   }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => Cons(h, () => t().takeWhile(p))
+    case _ => Empty
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)(p(_) && _)
+
+  def takeWhile1(p: A => Boolean): Stream[A] =
+    foldRight(Stream.empty[A])((el, str) =>
+      if (p(el)) Cons(() => el, () => str)
+      else Empty)
+
+  def headOption1: Option[A] =
+    foldRight()
+
 
 }
 
--- src/main/scala/functionalex/part1/StrictnessAndLaziness.scala
+++ src/main/scala/functionalex/part1/StrictnessAndLaziness.scala
@@ -4,7 +4,14 @@ package functionalex.part1
  * Created by Gleb on 2/26/2015.
  */
 object StrictnessAndLaziness {
-  def toList[A](st: Stream[A]): List[A] = {
-    ???
+
+  def main(args: Array[String]) {
+    val as = Stream(1, 2, 3, 4)
+    println(as.take(2).toList)
+    println(as)
+    println(as.toList)
+    println("should be List(2,3,4):  " + as.drop(1).toList)
+    println(as.takeWhile1(_ < 3).toList)
+    println(as.headOption)
   }
 }
