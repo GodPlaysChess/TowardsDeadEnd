2442170ee3dfc3900baee609d662dd97f06888df
GodPlaysChess <pumabox@gmail.com>
2015-03-03 11:07:39 +0100

    cleanup

--- src/main/scala/functionalex/part1/FunctionalState.scala
+++ src/main/scala/functionalex/part1/FunctionalState.scala
@@ -2,46 +2,10 @@ package functionalex.part1
 
 object FunctionalState {
 
-  // better to do map on tuple
-  def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (a, r) = rng.nextInt
-    if (a == Int.MinValue) (Int.MaxValue, r)
-    else (Math.abs(a), r)
-  }
 
-  def double(rng: RNG): (Double, RNG) = {
-    val (a, r) = nonNegativeInt(rng)
-    (a.toDouble / Int.MaxValue, r)
-  }
 
-  def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val (i, r1) = rng.nextInt
-    val (d, r2) = double(r1)
-    ((i, d), r2)
-  }
-
-  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-    val (d, r1) = double(rng)
-    val (i, r2) = r1.nextInt
-    ((d, i), r2)
-  }
-
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-    val (d1, r1) = double(rng)
-    val (d2, r2) = double(r1)
-    val (d3, r3) = double(r2)
-    ((d1, d2, d3), r3)
-  }
-
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    def innerInts(c: Int, l: List[Int], rng: RNG): (List[Int], RNG) = {
-      if (c == 0) (l, rng)
-      else {
-        val (i, r) = rng.nextInt
-        innerInts(c - 1, i :: l, r)
-      }
-    }
-    innerInts(count, List.empty, rng)
+  def main(args: Array[String]) {
+//    ints(5)(new SimpleRNG(212l))._1.foreach(println)
   }
 
 
--- src/main/scala/functionalex/part1/RNG.scala
+++ src/main/scala/functionalex/part1/RNG.scala
@@ -1,15 +1,77 @@
 package functionalex.part1
 
+
 trait RNG {
+  type Rand[+A] = RNG => (A, RNG)
+
   def nextInt: (Int, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (a, r) = rng.nextInt
+    if (a == Int.MinValue) (Int.MaxValue, r)
+    else (Math.abs(a), r)
+  }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def double: Rand[Double] = {
+    map(nonNegativeInt)(_.toDouble/Int.MaxValue)
+  }
+
+//  def double(rng: RNG): (Double, RNG) = {
+//    val (a, r) = nonNegativeInt(rng)
+//    (a.toDouble / Int.MaxValue, r)
+//  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r1) = double(rng)
+    val (i, r2) = r1.nextInt
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def innerInts(c: Int, l: List[Int], rng: RNG): (List[Int], RNG) = {
+      if (c == 0) (l, rng)
+      else {
+        val (i, r) = rng.nextInt
+        innerInts(c - 1, i :: l, r)
+      }
+    }
+    innerInts(count, List.empty, rng)
+  }
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
+
   override def nextInt: (Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
     val n = (newSeed >>> 16).toInt
     (n, nextRNG)
   }
-
 }
