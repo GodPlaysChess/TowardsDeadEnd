a8f486363a2f31cd93159da5c32a7141ac547ecd
GodPlaysChess <pumabox@gmail.com>
2015-03-15 22:47:10 +0100

    modified Prop

--- src/main/scala/functionalex/part2/Prop.scala
+++ src/main/scala/functionalex/part2/Prop.scala
@@ -1,11 +1,44 @@
 package functionalex.part2
 
-trait Prop {
-  def check: Boolean
+import functionalex.part1.RNG
+import functionalex.part1.Stream
+import functionalex.part1.Option
+import functionalex.part2.Prop.{FailedCase, SuccessCount, TestCases}
 
-  def &&(p: Prop): Prop =
-    new Prop {
-      def check = check && p.check
-    }
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+  type TestCases = Int
 
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def foraAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(as)(rng).zipWith(Stream.from(0))((_, _)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch {
+        case e: Exception => Falsified(buildMsg(a, e), i)
+      }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+}
+
+case class Prop(run: (TestCases, RNG) => Result)
+
+sealed trait Result {
+  def isFalsified: Boolean
+}
+
+case object Passed extends Result {
+  override def isFalsified = false
+}
+
+case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+  override def isFalsified = true
 }
