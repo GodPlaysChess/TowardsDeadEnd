020d38d312334c54cf157f612c6c4c223243df2f
GodPlaysChess <pumabox@gmail.com>
2015-03-24 21:38:49 +0100

    more functions on parsers

--- src/main/scala/functionalex/part2/Parsers.scala
+++ src/main/scala/functionalex/part2/Parsers.scala
@@ -1,12 +1,18 @@
 package functionalex.part2
 
+import functionalex.part1.Either
+
 import scala.language.implicitConversions
 
 // for any Char c : run(char(c))(c.toString) == Right(c)
 trait Parsers[ParseError, Parser[+ _]] {
   self =>
+
   def char(c: Char): Parser[Char] =
-    ???
+    string(c.toString) map (_.charAt(0))
+
+  def succeed[A](a: A): Parser[A] =
+    string("") map (_ => a)
 
   implicit def string(s: String): Parser[String]
 
@@ -14,22 +20,86 @@ trait Parsers[ParseError, Parser[+ _]] {
 
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
-  def run[A](p: Parser[A])(input: String): Either[ParseError, A] =
-    ???
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-  def orString(s1: String, s2: String): Parser[String] =
-    ???
+  def orString(s1: String, s2: String): Parser[String]
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A] =
-    ???
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
 
+  //don't really get how it should work. Does it have to return only matched fragments?
+  //if not, then it always returns the whole messge.
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  // i dont like the way to use listOfN with various N.
+  def zeroOrMore[A](c: Parser[A]): Parser[Int] =
+    slice(many(c)) map (_.size)
+
+  def oneOrMore[A](c: Parser[A]): Parser[Int]
+
+  def zeroAFolOneB[A](c1: Parser[A], c2: Parser[A]): Parser[(Int, Int)] =
+    zeroOrMore(c1) ** oneOrMore(c2)
+
+  def and[A](value: Parser[A], value1: => Parser[A]): Parser[A]
+
+  def together[A, B](value: Parser[A], value1: Parser[B]): Parser[(A, B)]
+
+  // counts given strings
+  def count[A](s: Parser[A]): Parser[Int]
+
+  def many[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _) or succeed(List())
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _)
+
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(g: (A, B) => C): Parser[C] =
+    (p ** p2) map { case (a, b) => g(a, b)} // (or g.tupled)
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
+    if (n == 0) succeed(List())
+    else map2(p, listOfN(n - 1, p))(_ :: _)
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
 
   case class ParserOps[A](p: Parser[A]) {
-    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+
+    def &[B >: A](p2: => Parser[B]): Parser[B] = self.and(p, p2)
+
+    def many: Parser[List[A]] = self.many(p)
+
+    def map[B](f: A => B) = self.map(p)(f)
+
+    def **[B](p2: => Parser[B]): Parser[(A, B)] = self.together(p, p2)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+  }
+
+  object Laws {
+
+    import functionalex.part2.Prop._
+    import functionalex.part1.Right
+
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(identity))(in)
+
+    /**
+     * Parser, which succeeds on Any String ('a') should succeed on Any string (s)
+     **/
+    def succeedLaw[A](in: Gen[(String, String)]): Prop =
+      forAll(in) { case (a, s) => run(succeed(a))(s) == Right(a)}
+
   }
 
 }
+
--- src/main/scala/functionalex/part2/ParsingMain.scala
+++ src/main/scala/functionalex/part2/ParsingMain.scala
@@ -5,8 +5,11 @@ package functionalex.part2
 object ParsingMain {
 
   def main(args: Array[String]): Unit = {
-    run(or(string("abra"), string("cadabra")))("abra") == Right("abra")
-    run(or(string("abra"), string("cadabra")))("abra") == Right("cadabra")
+//    run(or(string("abra"), string("cadabra")))("abra") == Right("abra")
+//    run(or(string("abra"), string("cadabra")))("abra") == Right("cadabra")
+//    run(listOfN(3, "ab" | "cad"))("ababcad") == Right("ababcad")
+//    run(listOfN(3, "ab" | "cad"))("cadabab") == Right("cadabab")
+//    run(listOfN(3, "ab" | "cad"))("ababab") == Right("ababab")
   }
 
 }
