ad617eec5e19846f0e2ab6f85326dc555549f433
GodPlaysChess <pumabox@gmail.com>
2015-09-01 15:24:59 +0200

    Added inelegant semifunctional algorithm

--- src/main/scala/algorithms/dynamic/LongestPalindrome.scala
+++ src/main/scala/algorithms/dynamic/LongestPalindrome.scala
@@ -1,6 +1,5 @@
 package algorithms.dynamic
 
-import scala.annotation.tailrec
 import scala.collection.mutable
 
 /**
@@ -18,26 +17,40 @@ object LongestPalindrome {
       else max(longestPalyndrome(h + mid), longestPalyndrome(mid + l))
     }
   }
-  
+
+  // and finally dynamic approach in a functional way
   def longestPalyndromeDynamic(in: String): String = {
-    val all = lpSub(in, 0, in.length, Map[(Int, Int), String]())
-    all(0 → in.length)
+    val all = lpSub(in, 0, in.length - 1, Map[(Int, Int), String]())
+    all(0 → (in.length - 1))
   }
 
 
   private def lpSub(in: String, s: Int, e: Int, all: Map[(Int, Int), String]): Map[(Int, Int), String] = {
-    if (in.isEmpty) all updated (s → e, in) // check if contains key
-    else if (in.length == 1) {
-      updateOrReturn(in, s, e, all)
-    }
+    if (s == e && !all.contains(s → e)) all updated (s → e, in.substring(s, e + 1))
     else {
-      val (h, mid, l) = split(in)
-      if (h == l) updateOrReturn(h + lpSub(mid, s, e, all) + l, s, e, all)
+      val h: String = in.charAt(s).toString
+      val l: String = in.charAt(e).toString
+      if (h == l) {
+        if (!all.contains((s + 1) → (e - 1))) {
+          val all1 = lpSub(in, s + 1, e - 1, all)
+          all1 updated (s → e, h + all1((s + 1) → (e - 1)) + l)
+        } else {
+          all updated (s → e, h + all((s + 1) → (e - 1)) + l)
+        }
+      }
       else {
-        all
-        //        max(longestPalyndrome(h + mid), longestPalyndrome(mid + l))
-        //      }
-      }}
+        var a = all
+        if (!all.contains((s + 1) → e)) {
+          val all1 = lpSub(in, s + 1, e, all)
+          a = all1
+        }
+        if (!all.contains(s → (e - 1))) {
+          val all2 = lpSub(in, s, e - 1, a)
+          a = all2
+        }
+        a updated (s → e, LongestPalindrome.max(a((s + 1) → e), a(s, e - 1)))
+      }
+    }
   }
 
   private def updateOrReturn(in: String, s: Int, e: Int, all: Map[(Int, Int), String]): Map[(Int, Int), String] = {
@@ -54,23 +67,39 @@ object LongestPalindrome {
   }
 
   def main(args: Array[String]) {
-    println(longestPalyndrome("character"))
-//    println(new LongestPalindrome().lpDynamic("character"))
+    println(longestPalyndrome("chaqracter"))
+    println(longestPalyndromeDynamic("chaqracter"))
     val L = new LongestPalindrome()
-    println(L.lpDynamic(
-      """
-        |fds;fksdfphello
-        | */
-        |public class DateModel extends DefaultReinitializableModel<Calendar> {
-        |	private final DateFormat dateFormat;
-        |
-        |	public DateModel(Calendar initialValue, @Nonnull DateFormat dateFormat) {
-        |		super(initialValue);
-        |		this.dateFormat = checkNotNull(dateFormat, "dateFormat");
-
-        |
-      """.stripMargin))
+    println(L.lpDynamic("chaqracter"))
   }
+
+  val sinput =  """
+                  |fds;fksdfphello
+                  | */
+                  |public class DateModel extends DefaultReinitializableModel<Calendar> {
+                  |	private final DateFormat dateFormat;
+                  |
+                  |	public DateModel(Calendar initialValue, @Nonnull DateFormat dateFormat) {
+                  |		super(initialValue);
+                  |		this.dateFormat = checkNotNull(dateFormat, "dateFormat");
+                  |	private final DateFormat dateFormat;
+                  |
+                  |	public DateModel(Calendar initialValue, @Nonnull DateFormat dateFormat) {
+                  |		super(initialValue);
+                  |		this.dateFormat = checkNotNull(dateFormat, "dateFormat");
+                  |	private final DateFormat dateFormat;
+                  |
+                  |	public DateModel(Calendar initialValue, @Nonnull DateFormat dateFormat) {
+                  |		super(initialValue);
+                  |		this.dateFormat = checkNotNull(dateFormat, "dateFormat");
+                  |	private final DateFormat dateFormat;
+                  |
+                  |	public DateModel(Calendar initialValue, @Nonnull DateFormat dateFormat) {
+                  |		super(initialValue);
+                  |		this.dateFormat = checkNotNull(dateFormat, "dateFormat");
+                  |
+                  |
+                """.stripMargin
 
 }
 
@@ -78,12 +107,12 @@ class LongestPalindrome {
   val all: scala.collection.mutable.HashMap[(Int, Int), String] = mutable.HashMap.empty
 
   def lpDynamic(in: String): String = {
-    longestPalindrome(0, in.length - 1, in)                                                               // ?
+    longestPalindrome(0, in.length - 1, in)
     all(0 → (in.length -1) )
   }
 
   private def longestPalindrome(s: Int, e: Int, in: String): Unit = {
-    if (s == e || s + 1 == e && !all.contains(s → e)) all put (s → e, in.substring(s, e + 1))
+    if (s == e && !all.contains(s → e)) all put (s → e, in.substring(s, e + 1))
     else {
       val h: String = in.charAt(s).toString
       val l: String = in.charAt(e).toString
