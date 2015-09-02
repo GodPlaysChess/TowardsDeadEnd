3bb4a523d75f50ee1d125d04db3b46730b52d9c8
GodPlaysChess <pumabox@gmail.com>
2015-09-02 22:36:25 +0200

    done with printing neatly

--- src/main/scala/algorithms/dynamic/PrintingNeatly.scala
+++ src/main/scala/algorithms/dynamic/PrintingNeatly.scala
@@ -2,9 +2,14 @@ package algorithms.dynamic
 
 /**
  * 02-Sep-2015.
+ * Working but dirty solution. Does not have time and desire to clean it up.
+ * since algorithm still holds.
+ * This algorithm has square complexity, but in my awesome implementation it takes n^3^.
+ * #reorginize last two lines can take linear time, so overall time would be n^2^
  */
 class PrintingNeatly {
   //Sum of the cubes of characters left is min (Except the last line)
+  //
 
   /*
   Some thoughts about the algorithm:
@@ -20,43 +25,72 @@ class PrintingNeatly {
 
       When 7 is added and does not fit, we must shift 4 to the next line.
       Essentially it happens, because nth line becomes n-1th line, and suddenly begins to add a score
+
   * */
 
 
-  val solution: scala.collection.mutable.Map[Int, Seq[Seq[String]]] = scala.collection.mutable.Map.empty
+  val solution: scala.collection.mutable.Map[Int, Seq[Seq[Int]]] = scala.collection.mutable.Map.empty
 
-
-  def printingNeatly(words: IndexedSeq[String], length: Int): Seq[Seq[String]] = {
-    ???
+  // Algorithm invokes n recurrences, each takes n^2 , so n^3 - overral time complexity
+  def printingNeatly(words: IndexedSeq[Int], length: Int): Seq[Seq[Int]] = {
+    sub(words, words.length - 1, length)
+    solution(words.length - 1)
+  }
+                             // O(n^2)
+  private def reorganizeLastTwoLines(s1: Seq[Int], s2: Seq[Int], lineSize: Int): (Seq[Int], Seq[Int]) = {
+    val all = s2 ++ s1
+    (1 to all.length - 1) map all.splitAt minBy (l ⇒ pointsOf2Sequences(l._1, l._2, lineSize))  // map takes O(n), then for every N -> we are calculating points (N)
   }
 
-  def sub(words: IndexedSeq[String], wordNum: Int, lineSize: Int): Unit = {
-    if (wordNum <= words.size) {
+  def sub(words: IndexedSeq[Int], wordNum: Int, lineSize: Int): Unit = {
+    if (wordNum == 0) solution.put(0, Seq(Seq(words.head)))                                                    // const
+    else {
       val word = words(wordNum)
-      if (!solution.contains(wordNum - 1)) sub(words, wordNum - 1, lineSize) // fill the subproblem solution
-      val lastLine = solution(wordNum - 1).last
-      if (word.length < spaceLeft(lastLine, lineSize)) {
-        // just append the word to the end of the last string
-        solution.put(wordNum, solution(wordNum - 1) map (_ :+ word))
+      if (!solution.contains(wordNum - 1)) sub(words, wordNum - 1, lineSize)
+      val prevSolution: Seq[Seq[Int]] = solution(wordNum - 1)
+      // fill the subproblem solution
+      val lastLine = prevSolution.last                                                                         // const for Vec
+      val linesAmount = prevSolution.size                                                                      // const
+      if (word < spaceLeft(lastLine, lineSize)) {
+        // just append the word to the end of the last Int
+        solution.put(wordNum, prevSolution updated (linesAmount - 1, lastLine :+ word))
       } else {
         // if it does not fit, do regrouping based on point criterion
         val veryLastLine = Seq(word)
-        val lastLine = solution(wordNum - 1).last
-        val previousLine = solution(wordNum - 1).init.last
+        if (linesAmount > 1) {
+          val previousLine: Seq[Int] = prevSolution(linesAmount - 2)
+          val (betterPrev, betterLast) = reorganizeLastTwoLines(lastLine, previousLine, lineSize)
+          val rearrangedSolution = (prevSolution updated (linesAmount - 2, betterPrev) updated (linesAmount - 1, betterLast)) :+ veryLastLine
+          solution.put(wordNum, rearrangedSolution)
+        } else {
+          solution.put(wordNum, lastLine +: Seq(veryLastLine))
+        }
 
-        val rearrangedSolution: Seq[Seq[String]] = ???
-        solution.put(wordNum, rearrangedSolution)
       }
     }
   }
 
-  def points(words: Seq[Seq[String]], lineSize: Int): Double = {
-   words.reverse.tail.foldLeft(0d)((score, seq) ⇒ score + math.pow(seq.reduce(_.length + 1 + _.length), 3))
+  def pointsOf2Sequences(l1: Seq[Int], l2: Seq[Int], lineSize: Int): Double = {
+    points(Seq(l1, l2, Seq.empty), lineSize)
+  }
+                                    // O(N)
+  def points(words: Seq[Seq[Int]], lineSize: Int): Double = {
+   words.reverse.tail.foldLeft(0d)((score, seq) ⇒ score + math.pow(spaceLeft(seq, lineSize), 3))
   }
 
-  private def spaceLeft(words: Seq[String], lineSize: Int): Int = {
-    lineSize - words.reduce(_.length + 1 + _.length)
+  private def spaceLeft(words: Seq[Int], lineSize: Int): Int = {
+    lineSize - (words reduce(_ + _ + 1))
   }
 
 
+}
+
+object PrintingNeatly {
+  def main(args: Array[String]) {
+    val input = Vector(2, 2, 4, 4, 6)
+    val P = new PrintingNeatly()
+    val solution = P.printingNeatly(input, 10)
+    println(solution)
+    println(P.points(solution, 10))
+  }
 }
